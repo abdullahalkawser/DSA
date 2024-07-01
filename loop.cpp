7d010fccd787e796a0ea58e1ac356854d9d5ecb7
Abdullah Al Kawser <abdullahalkawser1@gmail.com>
2024-07-01 23:14:28 +0600

    learn for loop while loop for-each nested loop

--- loop.cpp
+++ loop.cpp
@@ -2,7 +2,29 @@
 using namespace std;
 
 int main() {
-  for (int i = 0; i < 5; i++) {
+
+    // nesteed loop
+  // Outer loop
+  for (int i = 1; i <= 2; ++i) {
+    cout << "Outer: " << i << "\n";  // Executes 2 times
+    
+    // Inner loop
+    for (int j = 1; j <= 3; ++j) {
+      cout << " Inner: " << j << "\n";  // Executes 6 times (2 * 3)
+    }
+  }
+
+  // while loop
+
+    int i = 0;
+  while (i < 5) {
+    cout << i << "\n";
+    i++;
+  }
+
+// for each
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+  for (int i : myNumbers) {
     cout << i << "\n";
   }
   return 0;
